f014603219cad88e14b9865089ad2b52c036225b
Bhashini123868 <bhashimunasinghe286@gmail.com>
2024-11-26 20:31:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const chatList = [];
+
+let user = "";
+
+document.getElementById("selectUser").addEventListener("change", function () {
+  user = this.value;
+  console.log(this.value);
+});
+function sendMassage() {
+  let txtUserInput = document.getElementById("txtUserInput").value;
+  let chatBubble = "";
+  if (user === "Me") {
+    chatBubble = `<h3 class="text-end bg-success text-white ">${txtUserInput}</h3>`; 
+  } else {
+    chatBubble = `<h3 class="text-end bg-success text-white">${txtUserInput}</h3>`;
+    chatList.push(chatBubble);
+    loadChatBox();
+
+    console.log("Hello");
+  }
+  // --------------------------------------------------------------
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": txtUserInput
+          }
+        ]
+      }
+    ]
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+  };
+  fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=YOUR_API_KEY", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      
+      console.log(result.candidates[0].content.parts[0].text);
+      chatBubble=`<h3 class="text-start bg-secondary text-white">${result.candidates[0].content.parts[0].text}</h3>`;
+      chatList.push(chatBubble);
+      loadChatBox();
+    })
+    .catch((error) => console.error(error));
+  // --------------------------------------------------------------
+  loadChatBox();
+}
+function loadChatBox() {
+  document.getElementById("chatBox").innerHTML = "";
+  chatList.forEach(element => {
+    document.getElementById("chatBox").innerHTML += element;
+  })
+}
+
+// ----------------------------------------------------------------------
+
